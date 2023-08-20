0f2003f827c5d1fa2774599f43183e62596ef632
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-20 23:56:13 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RXJS-distinctuntilkeychanged-2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-distinctuntilkeychanged-xvmxys)
---
+++ index.ts
@@ -0,0 +1,13 @@
+console.clear();
+import { fromEvent } from 'rxjs';
+import { distinctUntilKeyChanged, pluck } from 'rxjs/operators';
+
+// https://www.learnrxjs.io/learn-rxjs/operators/filtering/distinctuntilkeychangedd
+
+// push buttons on keyboard
+const keys$ = fromEvent(document, 'keyup').pipe(
+  distinctUntilKeyChanged<KeyboardEvent>('code'),
+  pluck('key')
+);
+
+keys$.subscribe(console.log);
